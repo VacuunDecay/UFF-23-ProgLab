0b17977211b526d9ac2ddbdfbb0dd575eb32ff07
VacuunDecay <82185824+VacuunDecay@users.noreply.github.com>
2023-06-30 20:11:00 -0300

    1272
    feito

--- 1272.py
+++ 1272.py
@@ -1,10 +1,13 @@
 n = int(input())
 
+out = ""
+
 for i in range(n):
-    inp = int(input())
-    if(inp == 0):
-        print("Zero")
-    elif(inp == 1):
-        print("One")
-    else:
-        print("Invalid")
+    line = input()
+    if(len(line) != 0 and line[0] != ' '):
+        out += line[0]
+    for j in range(len(line)-1):
+        if(line[j] == ' ' and line[j+1] != ' '):
+            out += line[j+1]
+    print(out)
+    out = ""
